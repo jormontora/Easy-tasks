b985b0852f81acdf44776421608c6de76a4527eb
jormontora <ruslan.ruslan.kiev@gmail.com>
2025-02-13 21:17:10 +0200

    2 задача

---
+++ 2.cpp
@@ -0,0 +1,11 @@
+#include <iostream> //вывод
+#include <string> //анализ строки(библиотека)
+
+int main() {
+    std::cout << "Enter the name: "; // выводим на экранчик std::cout - отвечает за вывод в терминал
+    std::string name; // создаем переменную
+    std::cin >> name; // забиваем переменную вводом из клавиатуры
+    std::cout << "Hello, " << name << "!" << std::endl; // endl - перенос строки (выводим на экранчик) 
+    return 0; // конец
+}
+
