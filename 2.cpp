0c46fe4b656a339b85b07225adc22198bf63a785
jormontora <ruslan.ruslan.kiev@gmail.com>
2025-02-16 21:13:10 +0200

    видалив непотрібні коментарі

--- 2.cpp
+++ 2.cpp
@@ -1,11 +1,10 @@
-#include <iostream> //вывод
-#include <string> //анализ строки(библиотека)
-
+#include <iostream> 
+#include <string> 
 int main() {
-    std::cout << "Enter the name: "; // выводим на экранчик std::cout - отвечает за вывод в терминал
-    std::string name; // создаем строку
+    std::cout << "Enter the name: "; 
+    std::string name; 
     std::cin >> name; // забиваем переменную вводом из клавиатуры
-    std::cout << "Hello, " << name << "!" << std::endl; // endl - перенос строки (выводим на экранчик) 
-    return 0; // конец
+    std::cout << "Hello, " << name << "!" << std::endl; 
+    return 0; 
 }
 
